5c623042c80f54544576917ff4b2917efeab737d
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2021-11-23 23:03:22 -0800

    Method to check for the smallest vertex cover

--- MinimumVertexCover.java
+++ MinimumVertexCover.java
@@ -1,1 +1,32 @@
+public class Utility {
+	
+    static boolean[] find(UndirectedGraph g)
+    
+    {
+        boolean[] resultArray = new boolean[g.size()];
+        int k = 0;
+        for (int i =0;i<g.size();i++)
+    {
+      if (g.has(i))
+      {
+    	  k = i;
+    	  break ;
+      }
+    }
+ int count = k;
+ k--;
+        
+        for(int i=0; i<g.size(); i++)
+        {
+            if(g.remove(i).has(k))
+            {
+                resultArray[i] = true;
+                count--;
+            }
+            if(count==0)
+                break;
+        }
+        return resultArray;
+    }
+}
 
